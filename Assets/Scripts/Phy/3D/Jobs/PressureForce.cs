3c1ab767a26f4706f94ba3f58239c1f9b429ae02
Mustenaka <mumten120@outlook.com>
2024-12-24 15:28:03 +0800

    添加其他几种力的Job模板

---
+++ Assets/Scripts/Phy/3D/Jobs/PressureForce.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace Assets.Scripts.Jobs
+{
+    [BurstCompile]
+    public struct PressureForce : IJobParallelFor
+    {
+        public void Execute(int index)
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Phy/3D/Jobs/ViscosityForce.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Jobs;
+
+namespace Assets.Scripts.Jobs
+{
+    public struct ViscosityForce : IJobParallelFor
+    {
+        public void Execute(int index)
+        {
+            
+        }
+    }
+}
