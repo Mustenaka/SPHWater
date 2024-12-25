29c82c5a09894ebe8f2331db8a1bc47fbb9b8b35
Mustenaka <mumten120@outlook.com>
2024-12-25 18:34:35 +0800

    temp save

--- Assets/Scripts/Phy/3D/Jobs/PressureForce.cs
+++ Assets/Scripts/Phy/3D/Jobs/PressureForce.cs
@@ -5,15 +5,41 @@ using Unity.Mathematics;
 
 namespace Assets.Scripts.Jobs
 {
+    /// <summary>
+    /// 压力计算
+    /// </summary>
     [BurstCompile]
     public struct PressureForce : IJobParallelFor
     {
         public NativeArray<float3> positions;       // 位置
         public NativeArray<float2> densities;       // 密度
 
+        public NativeReference<float> targetDensity;    // 目标密度
+        public NativeReference<float> pressureMultiplier;   // 压力系数
+
         public void Execute(int index)
         {
 
+        }
+
+        /// <summary>
+        /// 从密度计算压力
+        /// </summary>
+        /// <param name="density"></param>
+        /// <returns></returns>
+        private float PressureFromDensity(float density)
+        {
+            return (density - targetDensity.Value) * pressureMultiplier.Value;
+        }
+
+        /// <summary>
+        /// 计算临接压力
+        /// </summary>
+        /// <param name="nearDensity"></param>
+        /// <returns></returns>
+        private float NearPressureFromDensity(float nearDensity)
+        {
+            return nearDensity * pressureMultiplier.Value;
         }
     }
 }
